eb7fbd0e840453e41a3c63676f5c9a067f139d6c
vkstrm <mongomoves@gmail.com>
2020-07-12 14:16:04 +0200

    Remove the builder

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,9 @@ type Sets = sets::Sets;
  */
 pub fn transform_to_stdout(katakana_mode: bool, input_vec: Vec<String>) {
     let words = internal_transform(katakana_mode, input_vec);
-    print(&words);
+    for word in words {
+        println!("{}",word.kana);
+    }
 }
 
 /**
@@ -25,45 +27,19 @@ pub fn transform(katakana_mode: bool, input_vec: Vec<String>) -> Vec<String> {
 }
 
 fn internal_transform(katakana_mode: bool, input_vec: Vec<String>) -> Vec<word::Word> {
-    let hiragana_set: Sets = Sets::new(sets::KanaType::Hiragana);
-    let katakana_set: Sets = Sets::new(sets::KanaType::Katakana);
-
-    let builder: WordBuilder;
+    let set: Sets; 
     if katakana_mode {
-        builder = WordBuilder::new(katakana_set);
+        set = Sets::new(sets::KanaType::Katakana);
     } else {
-        builder = WordBuilder::new(hiragana_set);
+        set = Sets::new(sets::KanaType::Hiragana);
     }
 
     let mut words = word::init_word_collection(input_vec);
 
     for word in &mut words {
-        builder.transform(word);
+        word.set_tokens(tokens::interpret_tokens(&word.original));
+        word.set_kana(&set.kana_dict);
     }
 
     words
-}
-
-fn print(words: &Vec<word::Word>) {
-    for word in words {
-        println!("{}",word.kana);
-    }
-}
-
-struct WordBuilder {
-    set: Sets,
-}
-
-impl WordBuilder {
-
-    pub fn new(set: Sets) -> WordBuilder {
-        WordBuilder {
-            set: set
-        }
-    }
-
-    pub fn transform(&self, word: &mut word::Word) {
-        word.set_tokens(tokens::interpret_tokens(&word.original));
-        word.set_kana(&self.set.kana_dict);
-    }
 }
